9e22375b5113f1abc3a507c01e1be017db2e4edf
romiljainb <romiljainb@gmail.com>
2017-02-27 23:54:47 -0800

    Bank Accounts

--- fin/account.go
+++ fin/account.go
@@ -3,13 +3,47 @@ package fin
 import (
 	//"fmt"
 	"github.com/romiljainb/ecom/user"
+	"time"
 )
 
 
 type Bank struct {
 	ID int
 	Name string
-//	Accounts []Account
+	Accounts []BankAccount
 
 }
 
+type BankAccount struct {
+	ID                 int
+	Name               string
+	AcctType           string
+	Balance            float32
+	Interest           float32
+	PendingWithdrawals float32
+	PendingDeposit     float32
+	Transactions	[]Transaction
+
+}
+
+type Transaction struct {
+	ID          int
+	Name        string
+	PostedDate  time.Time //date/time posted on
+	Description string
+	TransactionType string
+	Amount float32
+
+}
+
+func (b* Bank) Authorize(p user.PayMethod) int{
+	AuthCode := 0
+
+
+
+	return AuthCode
+}
+
+//transfer
+//withdraw
+//deposit
